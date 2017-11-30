d20aad8d41fc773fe0b54b2a848bc16b01a94aac
ro <exit>
2017-11-30 17:03:54 +0100

    creados articulos iniciamos

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'articulos/new'
+
+  get 'articulos/index'
+
   get 'estaticas/home'
-
   get 'estaticas/index'
-
+  
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
